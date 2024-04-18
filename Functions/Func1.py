0243dc068987de937cf07cd5e9cc14ced468de26
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-04-18 19:14:26 +0530

    Daily 2 coding solving challenges

---
+++ Functions/Func1.py
@@ -0,0 +1,7 @@
+# Write a program to create a function that takes two arguments, name and age, and print their value.
+
+def func(name,age):
+    print(f"name:{name} ,age:{age}")
+
+func("rudra",20)
+
