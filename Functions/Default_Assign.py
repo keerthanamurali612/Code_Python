9eb452b3529063c6b2a03bf58724bde6bf759f7b
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-04-20 19:46:53 +0530

    default argument

---
+++ Functions/Default_Assign.py
@@ -0,0 +1,9 @@
+#Exercise 3: Create a function with a default argument
+
+def show_employee(name,salary=9000):
+    print (f"name:{name},salary:{salary}")
+
+
+show_employee("Ben", 12000)
+show_employee("Rudra")
+
