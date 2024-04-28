9eaab978e931481770a7502ba73e06f6b70c41b4
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-04-28 21:09:57 +0530

    cube of number

---
+++ Loops/cube_num.py
@@ -0,0 +1,6 @@
+#Exercise 3: Calculate the cube of all numbers from 1 to a given number
+
+
+input_number = 6
+for i in range(1,input_number+1):
+    print(f"Current Number is : {i}  and the cube is {i*i*i}")
