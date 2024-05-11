b64d118a33dc3a31a36055f6719c97f799e586ea
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-05-11 21:14:00 +0530

    class

--- Class and objects/person_details.py
+++ Class and objects/person_details.py
@@ -2,12 +2,24 @@
 # Include attributes like name, country and date of birth.
 # Implement a method to determine the person's age.
 
+from datetime import date
 class person:
-    def __init__(self,name,country):
+    def __init__(self,name,country,dob):
         self.name=name
         self.country=country
-    def display(self):
+        self.dob=dob
+
+    def calculate_age(self):
         print(f" Name:{self.name},Country:{self.country}")
 
-p1=person("Rudra","Berlin")
-p1.display()
+        today=date.today()
+        age=today.year-self.dob.year
+        if(today<date(today.year,self.dob.month,self.dob.day)):
+            age-=1
+        return age
+
+
+
+p1=person("Rudra","Berlin",date(2000,2,10))
+
+p1.calculate_age()
