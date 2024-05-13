a4131b25695b08b77e8145f2c294eb8fe3ff7186
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-05-13 19:45:56 +0530

    bank details

--- Class and objects/bank_details.py
+++ Class and objects/bank_details.py
@@ -1,13 +1,22 @@
 
 
 class Bank:
-    def __init__(self,account_num,account_name):
+    def __init__(self,account_num,account_name,balance):
         self.accountNum=account_num
         self.accountName = account_name
+        self.avaBal=balance
+
+    def balance_acc(self):
+        if self.avaBal<=100:
+            print ("withdraw")
+        else:
+            print("insufficent balance")
+
 
     def display(self):
        print (f"Account Number :{self.accountNum},Account Name :{self.accountName}")
 
 
-b1=Bank(45353535,"Rudra")
+b1=Bank(4676458757,"Rudra",150)
 b1.display()
+b1.balance_acc()
