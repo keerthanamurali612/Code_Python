0796e84f7a142b031fb94185fbed624e52c264d3
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-05-15 21:26:31 +0530

    update

---
+++ Class and objects/calculator.py
@@ -0,0 +1,14 @@
+#Write a Python program to create a calculator class. Include methods for basic arithmetic operations.
+
+class Calculate:
+
+    def add(num1,num2):
+        return num1+num2
+    def sub(num1,num2):
+        return num1-num2
+
+result1=Calculate.add(60,20)
+result2=Calculate.sub(60,20)
+
+print("The addition is:",result1)
+print("The Subtraction is:",result2)
