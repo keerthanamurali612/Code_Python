8ddba98b388cfb3fe26eb7130fa507d6ca46309e
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-05-15 21:29:01 +0530

    update

--- Class and objects/calculator.py
+++ Class and objects/calculator.py
@@ -7,8 +7,13 @@ class Calculate:
     def sub(num1,num2):
         return num1-num2
 
+    def mul(num1, num2):
+        return num1 * num2
+
 result1=Calculate.add(60,20)
 result2=Calculate.sub(60,20)
+result3=Calculate.mul(60,20)
 
 print("The addition is:",result1)
 print("The Subtraction is:",result2)
+print("The Multiplication is:",result3)
