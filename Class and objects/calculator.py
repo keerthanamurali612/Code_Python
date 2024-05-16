a5e94bf7f4e2d52bd720ce71f47b943c048a0f07
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-05-16 07:34:50 +0530

    update

--- Class and objects/calculator.py
+++ Class and objects/calculator.py
@@ -2,18 +2,30 @@
 
 class Calculate:
 
-    def add(num1,num2):
+    def add(self,num1,num2):
         return num1+num2
-    def sub(num1,num2):
+    def sub(self,num1,num2):
         return num1-num2
 
-    def mul(num1, num2):
+    def mul(self,num1, num2):
         return num1 * num2
 
-result1=Calculate.add(60,20)
-result2=Calculate.sub(40,10)
-result3=Calculate.mul(30,20)
+    def div(self,num1,num2):
+        if num2!=0:
+            return num1%num2
+        else:
+            return "can't divide zero"
+
+
+
+calculate=Calculate()
+
+result1=calculate.add(60,20)
+result2=calculate.sub(40,10)
+result3=calculate.mul(30,20)
+result4=calculate.div(30,20)
 
 print("The addition is:",result1)
 print("The Subtraction is:",result2)
 print("The Multiplication is:",result3)
+print("The Divison is:",result4)
