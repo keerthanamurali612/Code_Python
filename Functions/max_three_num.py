6dd6d7e1857ec81267382ebd1d6e35e72b9e45a3
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-05-18 07:18:29 +0530

    update

---
+++ Functions/max_three_num.py
@@ -0,0 +1,27 @@
+# Write a Python function to find the maximum of three numbers.
+
+def max_three_num(a,b,c):
+    if(a>b):
+        print("The maximum of three numbers is :" , a )
+    elif(b>c):
+        print("The maximum of three numbers is :", b)
+
+    else:
+        print("The maximum of three numbers is :" ,c )
+
+max_num=max_three_num(21230,41230,23260)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
