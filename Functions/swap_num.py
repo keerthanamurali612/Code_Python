438622513ed92c77e93d6f97128962aeda633e0e
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-05-20 18:50:50 +0530

    update

--- Functions/swap_num.py
+++ Functions/swap_num.py
@@ -3,10 +3,10 @@
 
 
 def swap_pos(list,pos1,pos2):
-    list(pos1),list(pos2)=list(pos1),list(pos2)
+    list(pos1),list(pos2)=list(pos2),list(pos1)
     return list
 
    list=[23,65,19,20]
    pos1,pos2=1,3
 
-print(swap_pos(list pos1-1,pos2-1))
+print(swap_pos(list,pos1-1,pos2-1))
