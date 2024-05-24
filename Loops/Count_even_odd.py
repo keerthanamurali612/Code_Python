2fcb1b2c9c1078dded2ed2a091c4d113768d4f7f
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-05-24 18:52:08 +0530

    update

--- Loops/Count_even_odd.py
+++ Loops/Count_even_odd.py
@@ -1,17 +1,21 @@
 #Count the number of even and odd numbers from a series of numbers
 
 
+a=[1,2,3,4,5,6,7,8,9]
 even_count=0
 odd_count=0
-for i in list:
+for i in a:
     if i%2==0:
-        even_count += 1
-        print("The even numbers is :",even_count)
-    elif i%2!=0:
-        odd_count +=1
-        print("The odd numbers is :", odd_count)
+        even_count=even_count + 1
+    else:
+        odd_count=odd_count +1
 
-list=[1,2,3,4,5,6,7,8,9,10]
+
+print("The even numbers is :",even_count)
+print("The odd numbers is :", odd_count)
+
+
+
 
 
 
