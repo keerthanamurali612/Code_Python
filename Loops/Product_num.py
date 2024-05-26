439a3b7fb61d3a2b078a180fdd5e6bba36f412ac
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-05-26 18:52:43 +0530

    update

--- Loops/Product_num.py
+++ Loops/Product_num.py
@@ -1,9 +1,20 @@
 #Calculate the product of elements in a list using a for loop:
 
-list=[2,3,4]
-product=2
+# list=[2,3,4]
+# product=2
+#
+# for i in list:
+#     total=product * i
+#
+# print(total)
 
-for i in list:
-    total=product * i
 
-print(total)
+def multiply(list):
+    for i in list:
+        result = result * i
+        return result
+
+
+
+num=multiply(list=[2,7,3])
+print(num)
