7b97e6c59d694fa507a76f9a4d54210e59026a66
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-05-28 21:47:58 +0530

    update

--- Functions/element_list.py
+++ Functions/element_list.py
@@ -1,9 +1,20 @@
 #Calculate the product of elements in a list using a for loop:
 
-list=[2,7,3]
+
 def multiply(list):
+    product=1
     for i in list:
         product = product * i
-        print(f"The product of elements in a list : {product}")
+    return product
 
-product=multiply()
+
+#create a list
+list=[1,2,7,3,5]
+
+#create an object
+product=multiply(list)
+
+#print the product
+print(f"The product of elements in a list : {product}")
+
+
