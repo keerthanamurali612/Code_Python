d80c40e4f81d1467d840be073ca567cbab00416f
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-06-01 22:00:16 +0530

    update

---
+++ Loops/Vowels_Cons.py
@@ -0,0 +1,8 @@
+# Write a  Python program to check whether an alphabet is a vowel or consonant.
+
+str="rudra"
+
+if(str=="a" or str=="e"  or str=="i"  or str=="o"  or str=="u"):
+    print("vowels")
+else:
+    print("consonant")
