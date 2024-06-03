07d296aa0f14da4aa2828739a0744e148573ee43
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-06-03 07:12:00 +0530

    update

--- Loops/Vowels_Cons.py
+++ Loops/Vowels_Cons.py
@@ -1,8 +1,10 @@
 # Write a  Python program to check whether an alphabet is a vowel or consonant.
 
-str="a"
+char=input("Enter the character")
 
-if(str=="a" or str=="e"  or str=="i"  or str=="o"  or str=="u"):
-    print("vowels")
+vowels=['a','e','i','o','u','A','E','I','O','U']
+
+if char in vowels:
+    print(f"The character {char} is vowels")
 else:
-    print("consonant")
+    print(f"The character {char} is consonant")
