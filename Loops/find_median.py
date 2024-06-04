565debb944ae7598e05f3c2c3abd51312f05956c
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-06-04 20:20:00 +0530

    update

---
+++ Loops/find_median.py
@@ -0,0 +1,12 @@
+# Write a Python program to find the median of three values.
+# Expected Output:
+# Input first number: 15
+# Input second number: 26
+# Input third number: 29
+# The median is 26.0
+
+first_number: 15
+second_number: 26
+third_number: 29
+
+
