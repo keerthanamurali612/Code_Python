67fd13ddf05b9c8e6c04c54f71652b03e0b0f6b2
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-06-05 19:53:07 +0530

    update

--- Loops/find_median.py
+++ Loops/find_median.py
@@ -5,8 +5,25 @@
 # Input third number: 29
 # The median is 26.0
 
-first_number: 15
-second_number: 26
-third_number: 29
+
+x=input("enter the first num")
+y=input("enter the second num")
+z=input("enter the third num")
+
+print("Median of the above three numbers -")
+
+if y < x and x < z:
+    print("the first num1 :",x)
+elif z < x and x < y:
+    print("the first num2 :",x)
+elif z < y and y < x:
+    print("the second num1 :",y)
+elif x < y and y < z:
+    print("the second num2 :",y)
+elif y < z and z < x:
+    print("the third num1 :",z)
+elif x < z and z < y:
+    print("the third num2 :",z)
+
 
 
