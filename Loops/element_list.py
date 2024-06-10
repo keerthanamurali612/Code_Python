dfe27944e62e4866cbeeb92905f58a27da908de7
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-06-10 20:12:55 +0530

    update

---
+++ Loops/element_list.py
@@ -0,0 +1,11 @@
+#Check if element exists in list in Python
+
+# Initializing list
+test_list = [1, 6, 3, 5, 3, 4]
+
+# Checking if 4 exists in list
+for i in test_list:
+    if(i == 4):
+        print("Element Exists")
+
+
